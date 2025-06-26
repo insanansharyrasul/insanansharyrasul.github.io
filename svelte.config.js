cde14221cb1278a49dd4878b3794881784b4a10e
insanansharyrasul <insanansryrasul21@gmail.com>
2025-06-26 18:59:15 +0700

    fix: update deployment workflow to support multiple build directories

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: null,
 			precompress: false,
 			strict: true
-
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : ''
 		}
 	}
 };
