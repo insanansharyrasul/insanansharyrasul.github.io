b2528bb8a16c4e7fbbc18e0f84b5f8a9ee0e6911
insanansharyrasul <insanansryrasul21@gmail.com>
2025-06-26 18:46:09 +0700

    fix: changed github deployment and fixed build things

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
